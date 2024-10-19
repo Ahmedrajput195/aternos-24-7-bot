6615e6dce73ab7faee5c2646e5bf9e29fed94370
Ahmedrajput195 <124665428+Ahmedrajput195@users.noreply.github.com>
2024-10-19 20:16:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ let data = JSON.parse(rawdata);
 var lasttime = -1;
 var moving = 0;
 var connected = 0;
-var actions = [ 'forward', 'back', 'left', 'right']
+var actions = [ 'forward', 'back', 'left', 'right', 'jump']
 var lastaction;
 var pi = 3.14159;
 var moveinterval = 2; // 2 second movement interval
 var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
-var host = data["ip"];
-var username = data["name"]
+var host = data["Friends_Era.aternos.me"];
+var username = data["AFK BOT"]
 var nightskip = data["auto-night-skip"]
 var bot = mineflayer.createBot({
   host: host,
